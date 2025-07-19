f592a9edad70a46a786faa238c2f8acc0c62c740
Anuragreddy-Naredla <anuragreddy1241@gmail.com>
2025-07-19 16:32:09 +0530

    added_the_init_files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+
+setup(name="document_portal_analysis_with_adv_RAG",
+      author="Anurag Reddy Naredla",
+      version="0.1",
+      packages=find_packages(),
+      )
